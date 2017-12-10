bf98a982e62deaf16dd97569bad87cfc7fb2a8be
Ukesh <godsfear@gmail.com>
2017-12-09 22:49:24 -0800

    Added new employee and structure logic

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,6 +4,19 @@ class EmployeesController < ApplicationController
   end
 
   def new
+    @emp = Employee.new
+  end
+
+  def create
+    @emp = Employee.new(employee_params)
+    @emp.save
+    redirect_to new_employee_path, notice: "Employee created."
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :structure_id)
   end
 
 end
--- app/controllers/structures_controller.rb
+++ app/controllers/structures_controller.rb
@@ -1,9 +1,22 @@
 class StructuresController < ApplicationController
 
   def new
+    @st = Structure.new
   end
 
   def index
   end
-  
+
+  def create
+    @st = Structure.new(structure_params)
+    @st.save
+    redirect_to new_structure_path, notice: "Structure created."
+  end
+
+  private
+
+  def structure_params
+    params.require(:structure).permit(:name, :bp, :da, :hra)
+  end
+
 end
